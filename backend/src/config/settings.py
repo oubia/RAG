7bb2d69264d2c345ab0aa904cac67f21f6f4e203
oubia <dev.oubia@gmail.com>
2025-06-10 14:37:24 +0200

    feat: path deletion

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -1,7 +1,7 @@
 import os
 
-CHROMA_DIR = "/Users/yasaman/Desktop/dataminin-project/RAG_Embeddings/Vectorestor"
-#CHROMA_DIR = os.getenv("CHROMA_DIR")
+# CHROMA_DIR = "/Users/yasaman/Desktop/dataminin-project/RAG_Embeddings/Vectorestor"
+CHROMA_DIR = os.getenv("CHROMA_DIR")
 #CHROMA_DIR = os.getenv("CHROMA_DIR","D:\homy\S9\data-text-mining\Vectorestor")
 OLLAMA_LLM_ENDPOINT = os.getenv("OLLAMA_LLM_ENDPOINT", "http://localhost:11434/")
 OLLAMA_EMBEDDINGS_ENDPOINT = os.getenv("OLLAMA_EMBEDDINGS_ENDPOINT", "http://localhost:11434/")
